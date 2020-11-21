84024904dbd58823c71569a635b01c7325cf0909
jordanrob709 <jordanrob709@gmail.com>
2020-11-21 20:16:57 +0300

    setup local server with json-server and base components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
+import MovieList from './components/MovieList'
+import Header from './components/Header'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [ movies, setMovies] = useState([])
+
+
+  return(
+    <div>
+      <Header />
+
+      <section>
+        <MovieList />
+      </section>
     </div>
-  );
+  )
+  
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div>
+            <h2>Catch A Flick</h2>
+            <ul>
+                <li>Movies</li>
+                <li>TV</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header;
